ab0e69c110d130c0669d11e1d0303a07be1ba489
Matt <MGeiger410@me.com>
2021-08-12 13:33:13 +0200

    Initial Commit

---
+++ Shared/ForkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForkingApp.swift
+//  Shared
+//
+//  Created by Matt de Young on 12.08.21.
+//
+
+import SwiftUI
+
+@main
+struct ForkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
